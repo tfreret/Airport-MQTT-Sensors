44387827df6c4dc2c253d69f19dc38e1e11beb85
antoine otegui <antoine.otegui@soprasteria.com>
2024-01-02 09:26:14 +0100

    fix acess to Id + Airport

--- cmd/sensors/pressureSensor/pressureSensor.go
+++ cmd/sensors/pressureSensor/pressureSensor.go
@@ -9,12 +9,13 @@ type PressureSensor struct {
 	sensor.Sensor
 }
 
-func (pSensor PressureSensor) GetActualizeMeasure() sensor.Measurement {
+func (pSensor *PressureSensor) GetActualizeMeasure() sensor.Measurement {
 	// TODO fetch from api or get from json
 	return sensor.Measurement{TypeMesure: "Pres", Value: 0.66, Timestamp: time.Now().Format(time.RFC3339)}
 }
 
-func NewPressureSensor(idSensor int, idAirport string) (pSensor PressureSensor) {
+func NewPressureSensor(idSensor int, idAirport string) *PressureSensor {
+	pSensor := &PressureSensor{}
 	pSensor.Sensor = sensor.NewSensor(pSensor, idSensor, idAirport)
 	return pSensor
 }
--- cmd/sensors/tempSensor/tempSensor.go
+++ cmd/sensors/tempSensor/tempSensor.go
@@ -9,12 +9,13 @@ type TempSensor struct {
 	sensor.Sensor
 }
 
-func (tSensor TempSensor) GetActualizeMeasure() sensor.Measurement {
+func (tSensor *TempSensor) GetActualizeMeasure() sensor.Measurement {
 	// TODO fetch from api or get from json
 	return sensor.Measurement{TypeMesure: "Temp", Value: 0.66, Timestamp: time.Now().Format(time.RFC3339)}
 }
 
-func NewTempSensor(idSensor int, idAirport string) (tSensor TempSensor) {
+func NewTempSensor(idSensor int, idAirport string) *TempSensor {
+	tSensor := &TempSensor{}
 	tSensor.Sensor = sensor.NewSensor(tSensor, idSensor, idAirport)
 	return tSensor
 }
