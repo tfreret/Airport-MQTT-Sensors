7500bccf18afbf02ed40ddef07239ab61a13f0e0
nathanfouere <nathanfouere@tutanota.com>
2024-01-13 18:25:42 +0100

    start working on random

--- cmd/sensors/tempSensor/tempSensor.go
+++ cmd/sensors/tempSensor/tempSensor.go
@@ -3,6 +3,7 @@ package main
 import (
 	"airport/internal/apiClient"
 	"airport/internal/config"
+	"airport/internal/randomSensor"
 	"airport/internal/sensor"
 	"fmt"
 	"log"
@@ -14,20 +15,25 @@ type TempSensor struct {
 }
 
 func (tSensor *TempSensor) GetActualizeMeasure() (sensor.Measurement, error) {
-	apiResponse, err := apiClient.GetApiResponse(config.CHECKWX_URL+tSensor.Params.Airport+"/decoded", tSensor.Api.Key)
-	if err != nil {
-		log.Printf("Erreur lors de l'obtention de la réponse de l'API : %v", err)
+	if config.USE_API {
+		apiResponse, err := apiClient.GetApiResponse(config.CHECKWX_URL+tSensor.Params.Airport+"/decoded", tSensor.Api.Key)
+		if err != nil {
+			log.Printf("Erreur lors de l'obtention de la réponse de l'API : %v", err)
 
-		return sensor.Measurement{}, fmt.Errorf("échec lors de l'obtention de la mesure : %w", err)
+			return sensor.Measurement{}, fmt.Errorf("échec lors de l'obtention de la mesure : %w", err)
+		}
+		if len(apiResponse.Data) == 0 {
+			return sensor.Measurement{}, fmt.Errorf("réponse de l'API invalide")
+		}
+		return sensor.Measurement{TypeMesure: "Temp", Value: apiResponse.Data[0].Temperature.Celsius, Timestamp: time.Now().UTC().Format(time.RFC3339)}, nil
+	} else {
+		return sensor.Measurement{TypeMesure: "Temp", Value: tSensor.NumberGenerator.GenerateRandomNumber(), Timestamp: time.Now().UTC().Format(time.RFC3339)}, nil
 	}
-	if len(apiResponse.Data) == 0 {
-		return sensor.Measurement{}, fmt.Errorf("réponse de l'API invalide")
-	}
-	return sensor.Measurement{TypeMesure: "Temp", Value: apiResponse.Data[0].Temperature.Celsius, Timestamp: time.Now().UTC().Format(time.RFC3339)}, nil
 }
 
 func NewTempSensor(config sensor.ConfigSensor) *TempSensor {
 	tSensor := &TempSensor{}
-	tSensor.Sensor = sensor.NewSensor(tSensor, config)
+	nbGenerator := randomSensor.NewNumberGenerator(0)
+	tSensor.Sensor = sensor.NewSensor(tSensor, config, *nbGenerator)
 	return tSensor
 }
