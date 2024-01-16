ad7bd20bde51e8e5ff4dd9e9d9266e265fc18835
antoine otegui <antoine.otegui@soprasteria.com>
2024-01-16 17:44:20 +0100

    refacto config

--- cmd/alertManager/configStruct.go
+++ cmd/alertManager/configStruct.go
@@ -4,13 +4,11 @@ import(
 	"airport/internal/mqttTools"
 )
 
-type ConfigMaxValue struct {
-	MaxWindValue float64    `mapstructure:"maxWindValue"`
-	MaxPresValue float64    `mapstructure:"maxPresValue"`
-	MaxTempValue float64    `mapstructure:"maxTempValue"`
-}
-
 type ConfigStruct struct {
 	Mqtt   mqttTools.ConfigMqtt		`mapstructure:"mqtt"`
-	MaxValue ConfigMaxValue			`mapstructure:"maxValue"`
+	MaxValue struct {
+		MaxWindValue float64    `mapstructure:"maxWindValue"`
+		MaxPresValue float64    `mapstructure:"maxPresValue"`
+		MaxTempValue float64    `mapstructure:"maxTempValue"`
+	}			`mapstructure:"maxValue"`
 }
--- internal/sensor/configStruct.go
+++ internal/sensor/configStruct.go
@@ -4,17 +4,13 @@ import(
 	"airport/internal/mqttTools"
 )
 
-type ConfigUtilities struct {
-	Frequency int    `mapstructure:"frequency"`
-	Airport   string `mapstructure:"airport"`
-}
-
-type ConfigApi struct {
-	Key string `mapstructure:"key"`
-}
-
 type ConfigSensor struct {
 	Mqtt   mqttTools.ConfigMqtt		`mapstructure:"mqtt"`
-	Params ConfigUtilities			`mapstructure:"sensor"`
-	Api    ConfigApi				`mapstructure:"api"`
+	Params struct {
+		Frequency int    `mapstructure:"frequency"`
+		Airport   string `mapstructure:"airport"`
+	}			`mapstructure:"sensor"` 
+	Api    struct {
+		Key string `mapstructure:"key"`
+	}				`mapstructure:"api"`
 }
